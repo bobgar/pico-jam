560046e86306f59fdfd8b71d378be7ab93cc76aa
Kevin Kohler <kevin.kohler@demiurgestudios.com>
2025-09-12 18:51:57 -0400

    First Pico 8 commit

---
+++ pico-jam.lua
@@ -0,0 +1,16 @@
+function _init()
+  x=64
+  y=64
+end
+
+function _update()
+  if (btn(0)) then x=x-1 end
+  if (btn(1)) then x=x+1 end
+  if (btn(2)) then y=y-1 end
+  if (btn(3)) then y=y+1 end
+end
+
+function _draw()
+  cls(5) 
+  circfill(x,y,7,14) 
+end
